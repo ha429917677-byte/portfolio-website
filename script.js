202904cdc52c99b6bcce0768b44a37660a4ad28e
ha429917677-byte <ha429917677@gmail.com>
2026-03-26 01:15:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Portfolio loaded successfully");
+console.log("Portfolio Loaded Successfully");
+
+// Smooth scroll effect
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
